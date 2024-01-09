ebe40561a9f0bb669abcd6f8f97c71741e6d03c4
Giafn <giafauzan11@gmail.com>
2024-01-09 11:07:50 +0700

    first commit

---
+++ database/migrations/2024_01_09_040257_create_orders_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id('id_order');
+            $table->unsignedBigInteger('id_film');
+            $table->unsignedBigInteger('id_status_pembayaran');
+            $table->text('nomor_duduk');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
